36d512c6eac4540327de491db18a007eac2cc405
diracristian <diracristian16@hotmail.com>
2025-08-29 14:21:51 -0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Link from "next/link";
+
+export default function Index(){
+    return(
+        <div>
+            <h1>Hola Camilo</h1>
+            <ul>
+                <li>
+                    
+                    <a href="/blog">Blob</a>
+                
+                </li>
+            </ul>
+        </div>
+    );
+}
