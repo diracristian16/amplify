520fe1ad7add8acd72c3e50fad57af20b28ba950
diracristian <diracristian16@hotmail.com>
2025-09-01 16:28:57 -0500

    segundo commit :fire:

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,33 @@
-import React from "react";
 import Link from "next/link";
 
-export default function Index(){
-    return(
-        <div>
-            <h1>Hola Camilo</h1>
-            <ul>
-                <li>
-                    
-                    <a href="/blog">Blob</a>
-                
-                </li>
-            </ul>
-        </div>
-    );
+function Card({ id, title, imgUrl }) {
+  return (
+    <div className="card">
+      <img src={imgUrl} alt={title} width={200} />
+      <h3>{title}</h3>
+      <Link href={`/blog/${id}`}>Leer más</Link>
+    </div>
+  );
+}
+
+export default function Index({ blogs }) {
+  return (
+    <div>
+      <h1>Hola Camilo</h1>
+      <ul>
+        <li><Link href="/blog">Blog</Link></li>
+      </ul>
+      <div>
+        {blogs.map((post) => (
+          <Card key={post.id} id={post.id} title={post.title} imgUrl={post.photo_url} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch("https://api.slingacademy.com/v1/sample-data/blog-posts");
+  const data = await res.json();
+  return { props: { blogs: data.blogs } };
 }
